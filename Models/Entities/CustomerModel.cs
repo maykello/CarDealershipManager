ad18837428c30a0644a392cac3014078d09058bc
Michał Mańka <kontgames122@gmail.com>
2026-04-11 20:25:02 +0200

    poprawka modelu Customer
    usuniecie limitów dla pól, migracja, update bazy

--- Models/Entities/CustomerModel.cs
+++ Models/Entities/CustomerModel.cs
@@ -13,32 +13,19 @@ namespace CarDealershipManager.Models.Entities
         [Key]
         public int Id { get; set; }
         public CustomerType Type { get; set; } = CustomerType.Individual;
-        [MaxLength(100)]
         public string? FirstName { get; set; }
-        [MaxLength(100)]
         public string? LastName { get; set; }
-        [MaxLength(150)]
         public string? CompanyName { get; set; }
-        [MaxLength(50)]
         public string? NationalIdNumber { get; set; }
-        [MaxLength(50)]
         public string? TaxId { get; set; }
-        [MaxLength(50)]
         public string? DocumentNumber { get; set; }
-        [MaxLength(30)]
         public string? PhoneNumber { get; set; }
-        [MaxLength(150)]
         [EmailAddress]
         public string? Email { get; set; }
-        [MaxLength(100)]
         public required string Country { get; set; }
-        [MaxLength(100)]
         public required string City { get; set; }
-        [MaxLength(20)]
         public string? PostalCode { get; set; }
-        [MaxLength(150)]
         public string? AddressLine1 { get; set; }
-        [MaxLength(150)]
         public string? AddressLine2 { get; set; }
         public ICollection<ContractModel> Contracts { get; set; } = new List<ContractModel>();
         public ICollection<ReservationModel> Reservations { get; set; } = new List<ReservationModel>();
